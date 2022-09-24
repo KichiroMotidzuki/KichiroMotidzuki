d0bcfb666f586909fc6e8ba72905f1873d09fdb8
Artem Okorokov <ar.okorokov@gmail.com>
2022-09-24 20:04:29 +0300

    my first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
